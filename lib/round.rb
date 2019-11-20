82b9d8a221774d1fe19b005372addd1744189cf8
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-19 22:52:18 -0700

    Add round methods

--- lib/round.rb
+++ lib/round.rb
@@ -5,7 +5,6 @@ class Round
     @deck = deck
     @turns = []
     @number_correct = 0
-    # @turn_number = 0
   end
 
   def current_card
@@ -19,7 +18,33 @@ class Round
     if new_turn.correct?
       self.number_correct += 1
     end
-    
     new_turn
+  end
+
+  def number_correct_by_category(category)
+    category_correct = 0
+
+    self.turns.select do |turn|
+      if turn.card.category == category && turn.correct?
+        category_correct += 1
+      end
+    end
+    category_correct
+  end
+
+  def percent_correct
+    (self.number_correct.to_f / self.turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    correct = self.number_correct_by_category(category)
+    total = 0
+
+    self.turns.each do |turn|
+      if turn.card.category == category
+        total += 1
+      end
+    end
+    (correct.to_f / total.to_f) * 100
   end
 end
