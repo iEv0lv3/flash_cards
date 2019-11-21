60ad6f76df7393035ce755736cf51ce6ef0ad207
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-21 00:09:53 -0700

    Add card generator class test

---
+++ test/card_generator_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require './lib/turn'
+require './lib/round'
+require './lib/card_generator'
+require 'pry'
+
+class CardGeneratorTest < Minitest::Test
+
+  def test_card_generator_exists
+    new_deck = CardGenerator.new("cards.txt")
+
+    assert_instance_of CardGenerator, new_deck
+  end
+
+  def test_card_generator_has_file_name
+    new_deck = CardGenerator.new("cards.txt")
+
+    assert_equal "cards.txt", new_deck.filename
+  end
+
+  def test_add_cards_opens_file_and_adds_cards
+    new_deck = CardGenerator.new("cards.txt")
+    binding.pry
+    assert new_deck.add_cards
+  end
+end
